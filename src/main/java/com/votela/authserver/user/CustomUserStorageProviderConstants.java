c19607cacf13c7850aa6c06f76c13ee527a5de9a
Mkhululi47 <mkhululi.mtshemla1@gmail.com>
2021-07-06 13:56:11 +0200

    #5 added keycloak server

---
+++ src/main/java/com/votela/authserver/user/CustomUserStorageProviderConstants.java
@@ -0,0 +1,9 @@
+package com.votela.authserver.user;
+
+public final class CustomUserStorageProviderConstants {
+    public static final String CONFIG_KEY_JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static final String CONFIG_KEY_JDBC_URL = "jdbc:sqlserver:votela.database.windows.net;databaseName=Votela";
+    public static final String CONFIG_KEY_DB_USERNAME = "votlaadmin";
+    public static final String CONFIG_KEY_DB_PASSWORD = "votelaadmin001_";
+    public static final String CONFIG_KEY_VALIDATION_QUERY = "validationQuery";
+}
